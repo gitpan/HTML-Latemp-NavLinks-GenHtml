094e509b2652618e4de94f878251aa9476641421
Shlomi Fish <shlomif@shlomifish.org>
2006-10-07 20:26:52 +0000

    Import of SHLOMIF/HTML-Latemp-NavLinks-GenHtml-0.1.8 from CPAN.
    gitpan-cpan-distribution: HTML-Latemp-NavLinks-GenHtml
    gitpan-cpan-version:      0.1.8
    gitpan-cpan-path:         SHLOMIF/HTML-Latemp-NavLinks-GenHtml-0.1.8.tar.gz
    gitpan-cpan-author:       SHLOMIF
    gitpan-cpan-maturity:     released

--- lib/HTML/Latemp/NavLinks/GenHtml.pm
+++ lib/HTML/Latemp/NavLinks/GenHtml.pm
@@ -3,6 +3,13 @@ package HTML::Latemp::NavLinks::GenHtml;
 use warnings;
 use strict;
 
+use base 'Class::Accessor';
+
+__PACKAGE__->mk_accessors(qw(
+    nav_links_obj
+    root
+    ));
+
 =head1 NAME
 
 HTML::Latemp::NavLinks::GenHtml - A module to generate the HTML of the 
@@ -10,7 +17,7 @@ navigation links.
 
 =cut
 
-our $VERSION = '0.1.7';
+our $VERSION = '0.1.8';
 
 =head1 SYNOPSIS
 
@@ -18,6 +25,76 @@ our $VERSION = '0.1.7';
 
     use base 'HTML::Latemp::NavLinks::GenHtml::ArrowImages';
     
+
+=head1 METHODS
+
+=head2 $specialised_class->new('param1' => $value1, 'param2' => $value2)
+
+Initialises the object.
+
+=cut
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    $self->_init(@_);
+    return $self;
+}
+
+sub _init
+{
+    my $self = shift;
+    my (%args) = @_;
+
+    $self->root($args{root});
+    $self->nav_links_obj($args{nav_links_obj});
+
+    return $self;
+}
+
+=head2 $obj->get_total_html()
+
+Calculates the HTML and returns it.
+
+=cut
+
+sub _get_buttons
+{
+    my $self = shift;
+
+    my @buttons =
+    (
+        {
+            'dir' => "prev",
+            'button' => "left",
+            'title' => "Previous Page",
+        },
+        {
+            'dir' => "up",
+            'button' => "up",
+            'title' => "Up in the Site",
+        },
+        {
+            'dir' => "next",
+            'button' => "right",
+            'title' => "Next Page",
+        },
+    );
+
+    foreach my $button (@buttons)
+    {
+        my $dir = $button->{'dir'};
+        if ($button->{'exists'} = exists($self->nav_links_obj->{$dir}))
+        {
+            $button->{'link_obj'} = $self->nav_links_obj->{$dir};
+        }
+    }
+
+    return \@buttons;
+}
+
 =head1 AUTHOR
 
 Shlomi Fish, C<< <shlomif@iglu.org.il> >>
--- lib/HTML/Latemp/NavLinks/GenHtml/Text.pm
+++ lib/HTML/Latemp/NavLinks/GenHtml/Text.pm
@@ -5,9 +5,57 @@ use warnings;
 
 use vars qw($nav_buttons_html);
 
-use base 'Class::Accessor';
+use base 'HTML::Latemp::NavLinks::GenHtml';
 
-__PACKAGE__->mk_accessors(qw(root nav_links));
+=head1 NAME
+
+HTML::Latemp::NavLinks::GenHtml::Text - A class to generate the text HTML of
+the navigation links.
+
+=head1 SYNOPSIS
+
+    my $obj = HTML::Latemp::NavLinks::GenHtml::Text->new(
+        root => $path_to_root,
+        nav_links_obj => $links,
+        );
+
+=head1 DESCRIPTION
+
+This module generates text navigation links. C<root> is the relative path to 
+the site's root directory. C<nav_links> are the navigation links' objects
+hash as returned by L<HTML::Widgets::NavMenu> or something similar.
+
+=head1 METHODS
+
+=head2 $obj->get_total_html()
+
+Calculates and returns the final HTML.
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif@iglu.org.il> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-html-latemp-navlinks-genhtml@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=HTML-Latemp-NavLinks-GenHtml>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2005 Shlomi Fish, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the MIT X11 license.
+
+=cut
+
+__PACKAGE__->mk_accessors(qw(
+    nav_links
+    root
+    ));
 
 use Template;
 
@@ -19,24 +67,7 @@ $Template::Stash::SCALAR_OPS->{ 'substr' } = sub {
     return substr($_[0], $_[1], $_[2]);
 };
 
-
-sub new
-{
-    my $class = shift;
-    my $self = {};
-    bless $self, $class;
-    $self->initialize(@_);
-    return $self;
-}
-
-sub initialize
-{
-    my $self = shift;
-    my %args = (@_);
-    %{$self} = %args;
-}
-
-sub get_nav_buttons_html
+sub _get_nav_buttons_html
 {
     my $self = shift;
 
@@ -44,7 +75,6 @@ sub get_nav_buttons_html
     
     my $with_accesskey = $args{'with_accesskey'};
 
-    my $nav_links = $self->nav_links();
     my $root = $self->root();    
 
     my $template = 
@@ -53,38 +83,10 @@ sub get_nav_buttons_html
             'POST_CHOMP' => 1,
         }
         );
-
-    my @buttons =
-    (
-        { 
-            'dir' => "prev", 
-            'button' => "left", 
-            'title' => "Previous Page",
-        },
-        { 
-            'dir' => "up", 
-            'button' => "up", 
-            'title' => "Up in the Site",
-        },
-        {
-            'dir' => "next", 
-            'button' => "right", 
-            'title' => "Next Page",
-        },
-    );
-
-    foreach my $button (@buttons)
-    {
-        my $dir = $button->{'dir'};
-        if ($button->{'exists'} = exists($nav_links->{$dir}))
-        {
-            $button->{'link'} = $nav_links->{$dir};
-        }
-    }
-    
+ 
     my $vars = 
     {
-        'buttons' => \@buttons,
+        'buttons' => $self->_get_buttons(),
         'root' => $root,
         'with_accesskey' => $with_accesskey,
     };
@@ -95,7 +97,7 @@ sub get_nav_buttons_html
 [% SET key = b.dir.substr(0, 1) %]
 <li>[
 [% IF b.exists %]
-<a href="[% HTML.escape(b.link) %]" title="[% b.title %] (Alt+[% key FILTER upper %])"
+<a href="[% HTML.escape(b.link_obj.direct_url()) %]" title="[% b.title %] (Alt+[% key FILTER upper %])"
 [% IF with_accesskey %]
 accesskey="[% key %]"
 [% END %]
@@ -116,7 +118,7 @@ sub get_total_html
     my $self = shift;
 
     return "<ul class=\"nav_links\">\n" .
-        $self->get_nav_buttons_html(@_) .
+        $self->_get_nav_buttons_html(@_) .
         "\n</ul>";
 }
 
